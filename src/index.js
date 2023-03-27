2911e42845c8cb9e0d3d7c1bd7bd7d4f27dc8b5a
SyvonenkoOleh <115041334+SyvonenkoOleh@users.noreply.github.com>
2023-03-27 06:31:04 +0300

    3

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ const createInfoMarkup = data => {
       <h2>${name.official}</h2>
       <p>Capital: ${capital}</p>
       <p>Population: ${population}</p>
-      <p>Languages: ${Object.values(languages)}</p>`
+      <p>Languages: ${Object.values(languages).join(", ")}</p>`
   );
 };
 
